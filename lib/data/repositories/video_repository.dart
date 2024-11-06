3357c4c1fc9229dec08a8f77a81251ba061707b3
kukuhsain <kukuhsain@gmail.com>
2024-11-06 16:31:05 +0700

    add VideoRepository

---
+++ lib/data/repositories/video_repository.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:simple_reels_flutter/domain/entities/video.dart';
+
+class VideoRepository {
+  // Method to fetch video list from API
+  Future<List<Video>> fetchVideoList() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://api.ulearna.com/bytes/all?page=1&limit=10&country=United+States'));
+
+      // Check if the request was successful (status code 200)
+      if (response.statusCode == 200) {
+        // Decode JSON response
+        List<dynamic> data = jsonDecode(response.body);
+        // Convert the JSON to a list of Video objects
+        return data.map((json) => Video.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load videos');
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch data: $e');
+    }
+  }
+}
