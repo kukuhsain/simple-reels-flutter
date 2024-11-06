6bf9ea5f65936fcc20f9b55a2c8ff7405fbaa023
kukuhsain <kukuhsain@gmail.com>
2024-11-06 15:47:43 +0700

    add dummy UI in HomeScreen

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -10,8 +10,37 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Hello'),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    height: 200,
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Title',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  const Text(
+                    'Description',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const Text('Author'),
+                  const Text('Duration'),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
