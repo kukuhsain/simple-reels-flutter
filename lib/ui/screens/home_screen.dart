c2167a4038e14f4eddf8b8b90d49ebb8b2659874
kukuhsain <kukuhsain@gmail.com>
2024-11-07 09:44:30 +0700

    improve UI to be more like Reels

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -31,42 +31,88 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Home'),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16),
+      // body: ListView.builder(
+      //   padding: const EdgeInsets.all(16),
+      //   itemCount: videos.length,
+      //   itemBuilder: (context, index) {
+      //     final video = videos[index];
+      //     return VideoCard(video: video);
+      //   },
+      // ),
+      body: PageView.builder(
         itemCount: videos.length,
+        scrollDirection: Axis.vertical,
         itemBuilder: (context, index) {
           final video = videos[index];
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(video.thumbCdnUrl ?? ''),
-                  const SizedBox(height: 16),
-                  Text(
-                    video.title ?? '',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  ListTile(
-                    leading: CircleAvatar(
-                      radius: 40,
-                      backgroundImage:
-                          NetworkImage(video.user?.profilePictureCdn ?? ''),
-                    ),
-                    title: Text(video.user?.fullname ?? ''),
-                    subtitle: Text(video.user?.username ?? ''),
-                  ),
-                  Text('Duration: ${video.duration ?? 0}'),
-                  Text('Like: ${video.totalLikes ?? 0}'),
-                  Text('Comment: ${video.totalComments ?? 0}'),
-                  Text('Share: ${video.totalShare ?? 0}'),
-                ],
-              ),
-            ),
-          );
+          return VideoCard(video: video);
         },
+      ),
+    );
+  }
+}
+
+class VideoCard extends StatelessWidget {
+  final Video video;
+
+  const VideoCard({super.key, required this.video});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: SizedBox(
+        child: Stack(
+          children: [
+            Center(
+              child: Image.network(video.thumbCdnUrl ?? ''),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: Colors.white54,
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                radius: 40,
+                                backgroundImage: NetworkImage(
+                                    video.user?.profilePictureCdn ?? ''),
+                              ),
+                              title: Text(video.user?.fullname ?? ''),
+                              subtitle: Text(video.user?.username ?? ''),
+                            ),
+                            Text(
+                              video.title ?? '',
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            Text('Duration: ${video.duration ?? 0}'),
+                            Text('Like: ${video.totalLikes ?? 0}'),
+                            Text('Comment: ${video.totalComments ?? 0}'),
+                            Text('Share: ${video.totalShare ?? 0}'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
